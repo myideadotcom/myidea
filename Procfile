63265e62e3c1a941eff3ecc794ef372dde0ead67
library-system <gozdur.sebastian.licencjat@gmail.com>
2017-03-05 18:52:46 +0100

    modified procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: npm run heroku
+working: npm run heroku
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express'),
 
 var app = express();
 var staticRoot = __dirname + '/dist/';
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 3000));
 app.use(express.static(staticRoot));
 
 app.use(function(req, res, next){
